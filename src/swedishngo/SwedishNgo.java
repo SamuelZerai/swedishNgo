15682885094a824372532b40dac6d89cd7008652
SamuelZerai <Szerai03@gmail.com>
2024-05-02 17:21:46 +0200

    Initial commit

---
+++ src/swedishngo/SwedishNgo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package swedishngo;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+
+/**
+ *
+ * @author szera
+ */
+public class SwedishNgo {
+    
+    private static InfDB idb;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try{
+            idb = new InfDB("ngo_2024", "3306", "root", "Adelinje123.");
+            new Inloggning(idb).setVisible(true);
+            System.out.println("funkar");
+            
+        } catch (InfException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+    
+}
